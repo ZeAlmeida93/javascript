936269ebf5279f3efb4576ab1463f0c11c8c82d6
Jose Almeida <gralhastudio@gmail.com>
2024-05-15 20:26:38 +0100

    Update IIFE

--- Ex_04_Funcoes Anonimas/anonymfunction.js
+++ Ex_04_Funcoes Anonimas/anonymfunction.js
@@ -1,5 +1,67 @@
-function ()
-{
 
+// SAY MY NAME!!!
+
+const teste = function() {
     console.log('José de Santiago Almeida Cázeres');
 }
+
+const teste2 = function() {
+
+console.log()
+return "José Almeida";
+
+
+};
+
+
+// DO MY MATH!!!
+
+/*
+const testesoma = function(a, b) {
+
+    return a + b;
+const a = 1;
+const b = 4;
+console
+
+
+}
+*/
+
+//  IIFE
+
+(function() {
+
+    console.log('José de Santiago Almeida Cázeres')
+
+})();
+
+// Console log da função toda
+
+console.log((function() {
+
+    return 'José de Santiago Almeida Cázeres3'
+
+})());
+
+
+(function(a,b) {
+    console.log(a + b);
+    
+
+   
+
+})(1,3);
+
+
+
+// Consoel log da função 
+
+
+console.log((function(a,b) {
+    return a + b;
+    
+
+   
+
+})(6,8));
