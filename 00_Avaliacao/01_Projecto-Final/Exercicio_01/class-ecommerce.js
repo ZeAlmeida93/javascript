a33068e2612af7b510b30dff0d6e57be136a29b1
Jose Almeida <gralhastudio@gmail.com>
2024-07-28 20:42:46 +0100

    update exe_01_Final-Project

---
+++ 00_Avaliacao/01_Projecto-Final/Exercicio_01/class-ecommerce.js
@@ -0,0 +1,73 @@
+const product01 = { id: 1, name: 'Leather boots', price: 100 };
+const product02 = { id: 2, name: 'Knitted Sweater', price: 200 };
+const product03 = { id: 3, name: 'Merino Wool Scarf', price: 300 };
+
+
+
+class Ecommerce {
+    constructor() {
+        this.products = [];
+        this.cart = [];
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    setProductPrice(productId, price) {
+        const product = this.products.find(p => p.id === productId);
+        if (product) {
+            product.price = price;
+        }
+    }
+
+    getAllProducts() {
+        return this.products;
+    }
+
+    getAllProductsNames() {
+        return this.products.map(p => p.name).join('; ');
+    }
+
+    getProductById(productId) {
+        return this.products.find(p => p.id === productId);
+    }
+
+    getProductByName(productName) {
+        return this.products.find(p => p.name === productName);
+    }
+
+    getProductsByPrice(initialPrice, finalPrice) {
+        return this.products.filter(p => p.price >= initialPrice && p.price <= finalPrice);
+    }
+
+    addProductToCart(product) {
+        this.cart.push(product);
+    }
+
+    getCart() {
+        return this.cart;
+    }
+
+    getCartTotalPrice() {
+        return this.cart.reduce((total, product) => total + product.price, 0);
+    }
+}
+
+
+const ecommerce = new Ecommerce()
+
+ecommerce.addProduct(product01)
+ecommerce.addProduct(product02)
+ecommerce.addProduct(product03)
+
+console.log(ecommerce.getAllProducts())
+
+console.log(ecommerce.getProductById(3))
+
+
+
+
+const id = parseInt(document.getElementById("product-id").value);
+const name = document.getElementById("product-name").value;
+const price = parseFloat(document.getElementById("product-price").value);
